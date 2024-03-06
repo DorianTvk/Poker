0cb3974342172732d4a4c33e79433fb9b7e2e6e0
Opilane TTHK <opilane@tthk.ee>
2024-03-06 13:21:29 +0200

    Poker
    Poker

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using PokerClickerV3;
+
+namespace YourNamespace
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        private void InitializeComponent()
+        {
+            // InitializeComponent meetod peaks sisaldama loogikat, et initsialiseerida rakenduse komponendid.
+            // Kuid see meetod ei ole vajalik, kui seda pole vaja kasutada.
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace PokerClickerV3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            // Lisa siia kood, mida soovid teha, kui nupule klõpsatakse
+        }
+    }
+}
